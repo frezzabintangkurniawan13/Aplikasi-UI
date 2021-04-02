015467425661a80518fb5b1060b3223aa5da2be2
Your Name <you@example.com>
2021-04-02 19:58:17 +0800

    update pertama

---
+++ app/src/androidTest/java/com/frezzabintang/aplikasilanjutan/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.frezzabintang.aplikasiui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.frezzabintang.aplikasilanjutan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
